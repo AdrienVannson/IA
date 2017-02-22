2bbcf609415c4908851c8eba3e8fb40674af85b0
Adrien VANNSON <adrien.vannson@gmail.com>
2017-02-22 13:44:20 +0100

    Complétion de la classe Tour

--- partie/Tour.cpp
+++ partie/Tour.cpp
@@ -4,3 +4,26 @@ Tour::Tour()
 {
 
 }
+
+
+// Situation de jeu
+SituationJeu Tour::situationJeu () const
+{
+    return m_situationJeu;
+}
+
+void Tour::setSituationJeu (const SituationJeu &nouvelleSituationJeu)
+{
+    m_situationJeu = nouvelleSituationJeu;
+}
+
+// Action
+Action Tour::action () const
+{
+    return m_action;
+}
+
+void Tour::setAction (const Action &nouvelleAction)
+{
+    m_action = nouvelleAction;
+}
--- partie/Tour.h
+++ partie/Tour.h
@@ -1,11 +1,35 @@
+/*
+ * Décrit un tour de jeu
+ */
+
 #ifndef TOUR_H
 #define TOUR_H
+
+#include "Action.h"
+#include "SituationJeu.h"
 
 
 class Tour
 {
+
 public:
-    Tour();
+    Tour ();
+
+    SituationJeu situationJeu () const;
+    void setSituationJeu (const SituationJeu &nouvelleSituationJeu);
+
+    Action action () const;
+    void setAction (const Action &nouvelleAction);
+
+
+private:
+
+    // Situation du jeu lors du tour
+    SituationJeu m_situationJeu;
+
+    // Action jouée par le joueur, qui permet d'obtenir la situation du tour suivant
+    Action m_action;
+
 };
 
 #endif // TOUR_H
