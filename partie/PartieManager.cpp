fae628d1ac2327f38d9cfe8126e80249f46eedf4
Adrien VANNSON <adrien.vannson@gmail.com>
2017-02-22 15:00:40 +0100

    Complétion de la classe PartieManager

--- partie/PartieManager.cpp
+++ partie/PartieManager.cpp
@@ -4,3 +4,15 @@ PartieManager::PartieManager()
 {
 
 }
+
+
+// Parties
+std::vector<Partie*> PartieManager::parties () const
+{
+    return m_parties;
+}
+
+void PartieManager::addPartie (Partie *nouvellePartie)
+{
+    m_parties.push_back(nouvellePartie);
+}
--- partie/PartieManager.h
+++ partie/PartieManager.h
@@ -1,11 +1,30 @@
+/*
+ * Gère les parties
+ *
+ * Les parties ajoutées ne peuvent plus être modifiées par la suite
+ */
+
 #ifndef PARTIEMANAGER_H
 #define PARTIEMANAGER_H
+
+#include <vector>
+
+#include "Partie.h"
 
 
 class PartieManager
 {
+
 public:
-    PartieManager();
+    PartieManager ();
+
+    std::vector<Partie*> parties () const;
+    void addPartie (Partie *nouvellePartie);
+
+
+private:
+    std::vector<Partie*> m_parties;
+
 };
 
 #endif // PARTIEMANAGER_H
