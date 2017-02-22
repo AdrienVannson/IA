a4416dc567075bcb71cbca14ab5458983ee6027a
Adrien VANNSON <adrien.vannson@gmail.com>
2017-02-22 22:32:08 +0100

    Création d'un menu pour quitter l'application

--- UI/MainWindow.cpp
+++ UI/MainWindow.cpp
@@ -1,13 +1,21 @@
 #include "MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
+MainWindow::MainWindow (QWidget *parent) :
     QMainWindow(parent)
 {
+    // Menu
+    QMenu *menuFichier = menuBar()->addMenu(tr("Fichier"));
+
+    QAction *actionQuitter = new QAction(tr("Quitter"), this);
+    connect(actionQuitter, SIGNAL(triggered(bool)), qApp, SLOT(quit()));
+
+    menuFichier->addAction(actionQuitter);
+
     QPushButton *boutton = new QPushButton("Commencer l'entrainement");
     setCentralWidget(boutton);
 }
 
-MainWindow::~MainWindow()
+MainWindow::~MainWindow ()
 {
 
 }
--- UI/MainWindow.h
+++ UI/MainWindow.h
@@ -1,7 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QApplication>
 #include <QMainWindow>
+#include <QMenuBar>
 #include <QPushButton>
 
 
