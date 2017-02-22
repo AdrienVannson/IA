48478361f3dd10af9e3c572df10f30bad722ea47
Adrien VANNSON <adrien.vannson@gmail.com>
2017-02-22 13:39:17 +0100

    Complétion de la classe PartieDetaillee

--- partie/PartieDetaillee.cpp
+++ partie/PartieDetaillee.cpp
@@ -1,6 +1,42 @@
 #include "PartieDetaillee.h"
 
-PartieDetaillee::PartieDetaillee()
+// Initialisation des membres statiques
+int PartieDetaillee::m_idSuivant = 0;
+
+
+PartieDetaillee::PartieDetaillee () :
+    m_id(m_idSuivant++)
 {
 
+}
+
+
+// ID
+int PartieDetaillee::id () const
+{
+    return m_id;
+}
+
+
+// IDs des participants
+std::vector<int> PartieDetaillee::idsIAs () const
+{
+    return m_idsIAs;
+}
+
+void PartieDetaillee::setIdsIAs (std::vector<int> nouveauIdsIAs)
+{
+    m_idsIAs = nouveauIdsIAs;
+}
+
+
+// Commentaire
+std::string PartieDetaillee::commentaire () const
+{
+    return m_commentaire;
+}
+
+void PartieDetaillee::setCommentaire (const std::string &nouveauCommentaire)
+{
+    m_commentaire = nouveauCommentaire;
 }
--- partie/PartieDetaillee.h
+++ partie/PartieDetaillee.h
@@ -1,11 +1,44 @@
+/*
+ * Partie décrite par plusieurs informations supplémentaires
+ */
+
 #ifndef PARTIEDETAILLEE_H
 #define PARTIEDETAILLEE_H
+
+#include <string>
+#include <vector>
+
+#include "Partie.h"
 
 
 class PartieDetaillee
 {
+
 public:
-    PartieDetaillee();
+    PartieDetaillee ();
+
+    int id () const;
+
+    std::vector<int> idsIAs () const;
+    void setIdsIAs (std::vector<int> nouveauIdsIAs);
+
+    std::string commentaire () const;
+    void setCommentaire (const std::string &nouveauCommentaire);
+
+
+
+private:
+    static int m_idSuivant;
+
+    // Identifiant (unique) de la partie
+    int m_id;
+
+    // IDs des IAs ayant participé à la partie
+    std::vector<int> m_idsIAs;
+
+    // Commentaire sur la partie (facilite le débuggage)
+    std::string m_commentaire;
+
 };
 
 #endif // PARTIEDETAILLEE_H
