c127b2208758a3ddcbe864801f082a28664a8165
Adrien VANNSON <adrien.vannson@gmail.com>
2017-02-22 13:49:24 +0100

    Complétion de la classe Action

--- partie/Action.cpp
+++ partie/Action.cpp
@@ -4,3 +4,15 @@ Action::Action()
 {
 
 }
+
+
+// ID du joueur
+int Action::idJoueur () const
+{
+    return m_idJoueur;
+}
+
+void Action::setIdJoueur (const int nouvelIdJoueur)
+{
+    m_idJoueur = nouvelIdJoueur;
+}
--- partie/Action.h
+++ partie/Action.h
@@ -1,11 +1,26 @@
+/*
+ * Décrit une action réalisée par un joueur
+ */
+
 #ifndef ACTION_H
 #define ACTION_H
 
 
 class Action
 {
+
 public:
-    Action();
+    Action ();
+
+    int idJoueur () const;
+    void setIdJoueur (const int nouvelIdJoueur);
+
+
+private:
+
+    // ID du joueur ayant réalisé l'action
+    int m_idJoueur;
+
 };
 
 #endif // ACTION_H
