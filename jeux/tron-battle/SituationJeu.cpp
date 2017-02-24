f13783dead96c439d082ee2159936756dd217c91
Adrien VANNSON <adrien.vannson@gmail.com>
2017-02-24 12:01:47 +0100

    Complétion de la classe SituationJeu

--- jeux/tron-battle/SituationJeu.cpp
+++ jeux/tron-battle/SituationJeu.cpp
@@ -1,7 +1,65 @@
 #include "SituationJeu.h"
 
 
-SituationJeu::SituationJeu ()
+SituationJeu::SituationJeu (const int nbJoueurs)
 {
+    // Initialisation: toutes les cellules sont noires
+    for (int iCellule=0; iCellule<NB_CELLULES; iCellule++) {
+        m_cellules[iCellule] = 0;
+    }
 
+    // Initialisation des positions des joueurs
+    for (int iJoueur=0; iJoueur<nbJoueurs; iJoueur++) {
+        m_positionsJoueurs.push_back(0);
+    }
+}
+
+
+
+// Convertions
+int SituationJeu::positionCellule (const int iLigne, const int iColonne)
+{
+    return iLigne*NB_COLONNES + iColonne;
+}
+
+
+
+// Couleurs des cellules
+char SituationJeu::cellule (const int iCellule) const
+{
+    return m_cellules[iCellule];
+}
+
+char SituationJeu::cellule (const int iLigne, const int iColonne) const
+{
+    return cellule(positionCellule(iLigne, iColonne));
+}
+
+
+void SituationJeu::setCellule (const int iCellule, const char nouvelleCouleur)
+{
+    m_cellules[iCellule] = nouvelleCouleur;
+}
+
+void SituationJeu::setCellule (const int iLigne, const int iColonne, const char nouvelleCouleur)
+{
+    setCellule(positionCellule(iLigne, iColonne), nouvelleCouleur);
+}
+
+
+
+// Position des joueurs
+int SituationJeu::positionJoueur (const int iJoueur) const
+{
+    return m_positionsJoueurs[iJoueur];
+}
+
+void SituationJeu::setPositionJoueur (const int iJoueur, const int nouvellePosition)
+{
+    m_positionsJoueurs[iJoueur] = nouvellePosition;
+}
+
+void SituationJeu::setPositionJoueur (const int iJoueur, const int nouvelleLigne, const int nouvelleColonne)
+{
+    setPositionJoueur(iJoueur, positionCellule(nouvelleLigne, nouvelleColonne));
 }
--- jeux/tron-battle/SituationJeu.h
+++ jeux/tron-battle/SituationJeu.h
@@ -6,6 +6,7 @@
 #define SITUATIONJEU_H
 
 #include <array>
+#include <vector>
 
 
 class SituationJeu
@@ -13,13 +14,37 @@ class SituationJeu
 
 public:
 
-    SituationJeu ();
+    static const int NB_LIGNES = 20;
+    static const int NB_COLONNES = 30;
+
+    static const int NB_CELLULES = NB_LIGNES * NB_COLONNES;
+
+
+    SituationJeu (const int nbJoueurs = 2);
+
+    // Convertions
+    static int positionCellule (const int iLigne, const int iColonne);
+
+    // Couleurs des cellules
+    char cellule (const int iCellule) const;
+    char cellule (const int iLigne, const int iColonne) const;
+
+    void setCellule (const int iCellule, const char nouvelleCouleur);
+    void setCellule (const int iLigne, const int iColonne, const char nouvelleCouleur);
+
+    // Position des joueurs
+    int positionJoueur (const int iJoueur) const;
+
+    void setPositionJoueur (const int iJoueur, const int nouvellePosition);
+    void setPositionJoueur (const int iJoueur, const int nouvelleLigne, const int nouvelleColonne);
 
 
 private:
 
-    static const int NB_LIGNES;
-    static const int NB_COLONNES;
+    // Description des cellules en binaire: 00 = vide, 01 = bleu, 10 = rouge
+    std::array<char, NB_CELLULES> m_cellules;
+    std::vector<int> m_positionsJoueurs;
+
 
 };
 
