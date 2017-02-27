0f18a963739cc2c7a9ae364037410eaee6369f14
Adrien VANNSON <adrien.vannson@gmail.com>
2017-02-27 20:59:28 +0100

    Complétion de la classe Action

--- jeux/CodinGame/Ghost-in-the-Cell/Action.cpp
+++ jeux/CodinGame/Ghost-in-the-Cell/Action.cpp
@@ -22,3 +22,34 @@ void Action::setIdJoueur (const int nouvelIdJoueur)
 {
     m_idJoueur = nouvelIdJoueur;
 }
+
+
+
+/*
+ * Type
+ */
+
+Action::Type Action::type () const
+{
+    return m_type;
+}
+
+void Action::setType (const Type nouveauType)
+{
+    m_type = nouveauType;
+}
+
+
+/*
+ * Informations
+ */
+
+int Action::information (const int iInformation) const
+{
+    return m_informations[iInformation];
+}
+
+void Action::setInformation (const int nouvelleValeur, const int iInformation)
+{
+    m_informations[iInformation] = nouvelleValeur;
+}
--- jeux/CodinGame/Ghost-in-the-Cell/Action.h
+++ jeux/CodinGame/Ghost-in-the-Cell/Action.h
@@ -5,11 +5,22 @@
 #ifndef ACTION_H
 #define ACTION_H
 
+#include <array>
+
 
 class Action
 {
 
 public:
+
+    enum Type
+    {
+        DEPLACEMENT, // info1 = source, info2 = cible, info3 = nbUnitées
+        BOMBE, // info1 = source, info2 = cible
+        AMELIORATION, // info1 = cible
+        ATTENTE
+    };
+
 
     Action ();
 
@@ -17,10 +28,26 @@ public:
     void setIdJoueur (const int nouvelIdJoueur);
 
 
+    // Type
+    Type type () const;
+    void setType (const Type nouveauType);
+
+    // Informations
+    int information (const int iInformation) const;
+    void setInformation (const int nouvelleValeur, const int iInformation);
+
+
 private:
 
     // ID du joueur ayant réalisé l'action
     int m_idJoueur;
+
+
+    // Type
+    Type m_type;
+
+    // Informations
+    std::array<int, 3> m_informations;
 
 };
 
