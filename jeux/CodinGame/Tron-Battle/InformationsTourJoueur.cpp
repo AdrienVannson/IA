1f8218c25f008aeecd774320959641796bf3df5a
Adrien VANNSON <adrien.vannson@gmail.com>
2017-03-15 15:26:12 +0100

    Création de la classe InformationsTourJoueur

---
+++ jeux/CodinGame/Tron-Battle/InformationsTourJoueur.cpp
@@ -0,0 +1,6 @@
+#include "InformationsTourJoueur.h"
+
+InformationsTourJoueur::InformationsTourJoueur ()
+{
+
+}
---
+++ jeux/CodinGame/Tron-Battle/InformationsTourJoueur.h
@@ -0,0 +1,24 @@
+/*
+ * Informations données à un joueur pour un tour de jeu
+ */
+
+#ifndef INFORMATIONSTOURJOUEUR_H
+#define INFORMATIONSTOURJOUEUR_H
+
+#include "SituationJeu.h"
+
+
+class InformationsTourJoueur
+{
+
+public:
+
+    InformationsTourJoueur ();
+
+
+private:
+
+
+};
+
+#endif // INFORMATIONSTOURJOUEUR_H
