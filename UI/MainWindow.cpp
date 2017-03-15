296d8f5c12a85a4def375bebad21f1a7582be2ab
Adrien VANNSON <adrien.vannson@gmail.com>
2017-03-15 15:59:27 +0100

    Affichage d'une fausse partie

--- UI/MainWindow.cpp
+++ UI/MainWindow.cpp
@@ -19,8 +19,27 @@ MainWindow::MainWindow (QWidget *parent) :
     setCentralWidget(boutton);
 
 
+    // Création d'une fausse partie (TODEL)
+    Partie *partie = new Partie;
+
+    SituationJeu sitPrecedante;
+
+    for (int iTour=0; iTour<15; iTour++) {
+        SituationJeu nouvelleSituation(sitPrecedante);
+
+        nouvelleSituation.setCellule(iTour, 1 + 2*iTour, SituationJeu::JOUEUR_1);
+
+        Tour tour;
+        tour.setSituationJeu(nouvelleSituation);
+        partie->addTour(tour);
+
+        sitPrecedante = nouvelleSituation;
+    }
+
+
     // Affichage des parties
     WidgetPartie *widgetPartie = new WidgetPartie;
+    widgetPartie->afficherPartie(partie);
 
     QDockWidget *dockPartie = new QDockWidget("Partie", this);
     dockPartie->setWidget(widgetPartie);
