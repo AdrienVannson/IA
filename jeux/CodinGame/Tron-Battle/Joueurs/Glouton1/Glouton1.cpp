17c8055b7299f003ec13193111ada45577e82031
Adrien VANNSON <adrien.vannson@gmail.com>
2017-03-29 21:27:30 +0200

    Amélioration de l'algorithme glouton

--- jeux/CodinGame/Tron-Battle/Joueurs/Glouton1/Glouton1.cpp
+++ jeux/CodinGame/Tron-Battle/Joueurs/Glouton1/Glouton1.cpp
@@ -1,7 +1,24 @@
 #include "Glouton1.h"
 
+
 Glouton1::Glouton1 ()
 {
+    for (int iLigne=0; iLigne<NB_LIGNES; iLigne++) {
+        for (int iColonne=0; iColonne<NB_COLONNES; iColonne++) {
+            estOccupe[iLigne][iColonne] = false;
+        }
+    }
+
+
+    for (int iColonne=0; iColonne<NB_COLONNES; iColonne++) {
+        estOccupe[0][iColonne] = true;
+        estOccupe[NB_LIGNES-1][iColonne] = true;
+    }
+
+    for (int iLigne=0; iLigne<NB_LIGNES; iLigne++) {
+        estOccupe[iLigne][0] = true;
+        estOccupe[iLigne][NB_COLONNES-1] = true;
+    }
 }
 
 Glouton1::~Glouton1 ()
@@ -10,12 +27,52 @@ Glouton1::~Glouton1 ()
 
 Action Glouton1::jouerAction (const InformationsTourJoueur &informations)
 {
-    Action action;
-    action.setDirection(Action::GAUCHE);
+    for (unsigned int iJoueur=0; iJoueur<informations.m_joueurs.size(); iJoueur++) {
+        std::pair<int, int> coordonnees = informations.m_joueurs[iJoueur].second;
 
-    if (informations.m_joueurs[informations.m_idJoueur].second.second == 0) {
-        action.setDirection(Action::BAS);
+        estOccupe[coordonnees.first+1][coordonnees.second+1] = true;
+    }
+
+
+    const int iLigne = informations.m_joueurs[informations.m_idJoueur].second.first + 1; // +1 pour prendre en compte les barrières
+    const int iColonne = informations.m_joueurs[informations.m_idJoueur].second.second + 1;
+
+
+    Action action;
+
+    for (int iDirection=0; iDirection<NB_DEPLACEMENTS; iDirection++) {
+        Action::Direction direction = (Action::Direction) iDirection;
+
+        if (direction == Action::GAUCHE && estPossible(iLigne, iColonne-1)) {
+            action.setDirection(Action::GAUCHE);
+        }
+
+        if (direction == Action::DROITE && estPossible(iLigne, iColonne+1)) {
+            action.setDirection(Action::DROITE);
+        }
+
+        if (direction == Action::HAUT && estPossible(iLigne-1, iColonne)) {
+            action.setDirection(Action::HAUT);
+        }
+
+        if (direction == Action::BAS && estPossible(iLigne+1, iColonne)) {
+            action.setDirection(Action::BAS);
+        }
     }
 
     return action;
+}
+
+
+bool Glouton1::estPossible (const int iLigne, const int iColonne) const
+{
+    if (iLigne < 0 || iLigne >= NB_LIGNES || iColonne < 0 || iColonne >= NB_COLONNES) {
+        return false;
+    }
+
+    if (estOccupe[iLigne][iColonne]) {
+        return false;
+    }
+
+    return true;
 }
--- jeux/CodinGame/Tron-Battle/Joueurs/Glouton1/Glouton1.h
+++ jeux/CodinGame/Tron-Battle/Joueurs/Glouton1/Glouton1.h
@@ -15,6 +15,19 @@ public:
     Action jouerAction (const InformationsTourJoueur &informations);
 
 
+private:
+
+    static const int NB_LIGNES = 20 + 2;
+    static const int NB_COLONNES = 30 + 2;
+
+    static const int NB_DEPLACEMENTS = 4;
+
+
+    bool estOccupe[NB_LIGNES][NB_COLONNES];
+
+
+    bool estPossible (const int iLigne, const int iColonne) const;
+
 };
 
 #endif // GLOUTON1_H
