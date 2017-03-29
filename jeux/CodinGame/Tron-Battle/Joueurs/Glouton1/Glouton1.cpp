0b90a3abfc91e551d882742b926eea8d4ddbd6c8
Adrien VANNSON <adrien.vannson@gmail.com>
2017-03-29 21:03:34 +0200

    Changement de direction lorsque le bord de la grille est atteind

--- jeux/CodinGame/Tron-Battle/Joueurs/Glouton1/Glouton1.cpp
+++ jeux/CodinGame/Tron-Battle/Joueurs/Glouton1/Glouton1.cpp
@@ -13,5 +13,9 @@ Action Glouton1::jouerAction (const InformationsTourJoueur &informations)
     Action action;
     action.setDirection(Action::GAUCHE);
 
+    if (informations.m_joueurs[informations.m_idJoueur].second.second == 0) {
+        action.setDirection(Action::BAS);
+    }
+
     return action;
 }
