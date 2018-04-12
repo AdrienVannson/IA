ffa7d93ddd9af9fd6db0547e81b72f1313978b05
Adrien VANNSON <adrien.vannson@gmail.com>
2018-04-12 20:19:59 +0200

    Utilisation de QProcess

--- joueurs/JoueurExterneAbstrait.cpp
+++ joueurs/JoueurExterneAbstrait.cpp
@@ -1,127 +1,34 @@
 #include "JoueurExterneAbstrait.hpp"
 
-
-/*
- * COMMUNICATION
- */
-
-// Wrapping pipe in a class makes sure they are closed when we leave scope
-class cpipe {
-private:
-    int fd[2];
-public:
-    inline int read_fd() const { return fd[0]; }
-    inline int write_fd() const { return fd[1]; }
-    cpipe() { if (pipe(fd)) throw std::runtime_error("Failed to create pipe"); }
-    void close() { ::close(fd[0]); ::close(fd[1]); }
-    ~cpipe() { close(); }
-};
-
-
-//
-// Usage:
-//   spawn s(argv)
-//   s.stdin << ...
-//   s.stdout >> ...
-//   s.send_eol()
-//   s.wait()
-//
-class spawn {
-private:
-    cpipe write_pipe;
-    cpipe read_pipe;
-public:
-    int child_pid = -1;
-    std::unique_ptr<__gnu_cxx::stdio_filebuf<char> > write_buf = NULL;
-    std::unique_ptr<__gnu_cxx::stdio_filebuf<char> > read_buf = NULL;
-    std::ostream stdin;
-    std::istream stdout;
-
-    spawn(const char* const argv[], bool with_path = false, const char* const envp[] = 0): stdin(NULL), stdout(NULL) {
-        child_pid = fork();
-        if (child_pid == -1) throw std::runtime_error("Failed to start child process");
-        if (child_pid == 0) {   // In child process
-            dup2(write_pipe.read_fd(), STDIN_FILENO);
-            dup2(read_pipe.write_fd(), STDOUT_FILENO);
-            write_pipe.close(); read_pipe.close();
-            int result;
-            if (with_path) {
-                if (envp != 0) result = execvpe(argv[0], const_cast<char* const*>(argv), const_cast<char* const*>(envp));
-                else result = execvp(argv[0], const_cast<char* const*>(argv));
-            }
-            else {
-                if (envp != 0) result = execve(argv[0], const_cast<char* const*>(argv), const_cast<char* const*>(envp));
-                else result = execv(argv[0], const_cast<char* const*>(argv));
-            }
-            if (result == -1) {
-               // Note: no point writing to stdout here, it has been redirected
-               std::cerr << "Error: Failed to launch program" << std::endl;
-               exit(1);
-            }
-        }
-        else {
-            close(write_pipe.read_fd());
-            close(read_pipe.write_fd());
-            write_buf = std::unique_ptr<__gnu_cxx::stdio_filebuf<char> >(new __gnu_cxx::stdio_filebuf<char>(write_pipe.write_fd(), std::ios::out));
-            read_buf = std::unique_ptr<__gnu_cxx::stdio_filebuf<char> >(new __gnu_cxx::stdio_filebuf<char>(read_pipe.read_fd(), std::ios::in));
-            stdin.rdbuf(write_buf.get());
-            stdout.rdbuf(read_buf.get());
-        }
-    }
-
-    void send_eof() { write_buf->close(); }
-
-    int wait() {
-        int status;
-        waitpid(child_pid, &status, 0);
-        return status;
-    }
-};
-
-/*
- * END COMMUNICATION
- */
-
-
-JoueurExterneAbstrait::JoueurExterneAbstrait (const std::string &chemin) :
-    m_chemin (chemin),
-    m_prog (0)
-{}
+JoueurExterneAbstrait::JoueurExterneAbstrait (const string &chemin) :
+    m_chemin (chemin)
+{
+    m_processus = new QProcess;
+}
 
 JoueurExterneAbstrait::~JoueurExterneAbstrait ()
 {
-    if (m_prog) {
-        m_prog->send_eof();
-        kill(m_prog->child_pid, SIGTERM);
-        m_prog->wait(); // Évite les processus zombies
-
-        delete m_prog;
-    }
+    m_processus->kill();
 }
 
 void JoueurExterneAbstrait::demarrerProg ()
 {
-    const char* const argv[] = {m_chemin.c_str(), (const char*)0};
-    m_prog = new spawn(argv);
+    m_processus->start(m_chemin.c_str());
 }
 
-
-
-std::string JoueurExterneAbstrait::getLine ()
+string JoueurExterneAbstrait::getLine ()
 {
-    cerr << "Lecture: " << m_prog->child_pid << endl;
-    string output;
-    getline(m_prog->stdout, output);
-    cerr << "Action recue (" << output.size() << "): " << output << endl;
-    return output;
+    char buffer[1024];
+    qint64 longueurLue = m_processus->readLine(buffer, sizeof(buffer));
+
+    if (longueurLue == -1) {
+        cerr << "Error" << endl;
+    }
+
+    return string (buffer);
 }
 
-void JoueurExterneAbstrait::send (const string &data)
+void JoueurExterneAbstrait::send (const string &donnees)
 {
-    cerr << "Infos envoyees:" << endl;
-    cerr << data;
-    cerr << "Fin infos" << endl;
-    m_prog->stdin << data;
-    m_prog->stdin.flush();
-    cerr << "Envoi OK" << endl;
+    m_processus->write(donnees.c_str());
 }
--- joueurs/JoueurExterneAbstrait.hpp
+++ joueurs/JoueurExterneAbstrait.hpp
@@ -1,40 +1,32 @@
 #ifndef JOUEUREXTERNEABSTRAIT_HPP
 #define JOUEUREXTERNEABSTRAIT_HPP
 
-#include <ext/stdio_filebuf.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include <QProcess>
+
 #include <iostream>
-#include <memory>
-#include <exception>
 #include <string>
 
 #include "partie/Joueur/Joueur.hpp"
 
-class spawn;
+using namespace std;
 
 
 class JoueurExterneAbstrait : public Joueur
 {
 
 public:
-
-    JoueurExterneAbstrait (const std::string &chemin="");
+    JoueurExterneAbstrait (const string &chemin);
     ~JoueurExterneAbstrait ();
-
-
-protected:
 
     void demarrerProg ();
 
-    std::string getLine ();
-    void send (const std::string &data);
-
+protected:
+    string getLine ();
+    void send (const string &donnees);
 
 private:
-
     std::string m_chemin;
-    spawn *m_prog;
+    QProcess *m_processus;
 
 };
 
